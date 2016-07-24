e70f00a6a6b356334b248e403f0ad687c621d075
Karthik Raja <rajakarthik131@gmail.com>
2016-07-24 21:16:19 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//fuction to strike text
+
+$("ul").on("click","li",function(){
+ $(this).toggleClass("strike");
+});
+
+
+
+//function to remove text on clicking tras icon
+$("ul").on("click", "span" ,function(){
+   $(this).parent().fadeOut(500,function(){
+       $(this).remove();
+   });
+  event.stopPropagation();
+});
+
+
+//function to add list when enter key is pressed on input
+$("input[type='text']").keypress(function(){
+  if(event.which === 13){
+     var ToDotxt= $(this).val();
+    $(this).val("");
+  $("ul").append("<li>"+ToDotxt+" <span><i class='fa fa-trash'></i></span></li>");    
+  }
+});
+
+
+//function to toggle input box when plus icon is pressed
+$(".fa-plus").on("click",function(){
+    $("input").fadeToggle();
+})
